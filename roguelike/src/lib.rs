52858e3918e9080f8dddb16868a1df86bf9104bc
James McNeill <mcneja@gmail.com>
2021-03-04 02:33:06 -0800

    Add VI key controls

--- roguelike/src/lib.rs
+++ roguelike/src/lib.rs
@@ -13,6 +13,14 @@ const KEY_LEFT: i32 = 37;
 const KEY_UP: i32 = 38;
 const KEY_RIGHT: i32 = 39;
 const KEY_DOWN: i32 = 40;
+const KEY_B: i32 = 66;
+const KEY_H: i32 = 72;
+const KEY_J: i32 = 74;
+const KEY_K: i32 = 75;
+const KEY_L: i32 = 76;
+const KEY_N: i32 = 78;
+const KEY_U: i32 = 85;
+const KEY_Y: i32 = 89;
 const KEY_NUMPAD1: i32 = 97;
 const KEY_NUMPAD2: i32 = 98;
 const KEY_NUMPAD3: i32 = 99;
@@ -76,14 +84,22 @@ fn draw_world(world: &World) {
 
 fn update_world(world: &mut World, key: i32, ctrl_key_down: bool, shift_key_down: bool) {
 	let vertical_offset =
-		if ctrl_key_down { -1 } else { 0 } +
-		if shift_key_down { 1 } else { 0 };
+		if ctrl_key_down {-1} else {0} +
+		if shift_key_down {1} else {0};
 
 	let modifier = match key {
 		KEY_LEFT => (-1, vertical_offset),
 		KEY_UP => (0, 1),
 		KEY_RIGHT => (1, vertical_offset),
 		KEY_DOWN => (0, -1),
+		KEY_B => (-1, -1),
+		KEY_H => (-1, 0),
+		KEY_J => (0, -1),
+		KEY_K => (0, 1),
+		KEY_L => (1, 0),
+		KEY_N => (1, -1),
+		KEY_U => (1, 1),
+		KEY_Y => (-1, 1),
 		KEY_NUMPAD1 => (-1, -1),
 		KEY_NUMPAD2 => (0, -1),
 		KEY_NUMPAD3 => (1, -1),
