f678648534cd11dad7bdfc222143f0a50954730a
James McNeill <mcneja@gmail.com>
2021-03-07 06:51:34 -0800

    Make the test text not be misleading

--- roguelike/src/game.rs
+++ roguelike/src/game.rs
@@ -81,7 +81,7 @@ pub fn on_draw(state: &State, screen_size_x: i32, screen_size_y: i32) {
 	engine::draw_rect(0, screen_size_y - BAR_HEIGHT, screen_size_x, BAR_HEIGHT, BAR_BACKGROUND_COLOR);
 	engine::draw_rect(0, 0, screen_size_x, BAR_HEIGHT, BAR_BACKGROUND_COLOR);
 
-	puts_proportional(4, screen_size_y - fontdata::LINE_HEIGHT + 4, "Press F1 for help", 0xffffffff);
+	puts_proportional(4, screen_size_y - fontdata::LINE_HEIGHT + 4, "Hello, world!", 0xffffffff);
 }
 
 pub fn on_key_down(state: &mut State, key: i32, ctrl_key_down: bool, shift_key_down: bool) {
